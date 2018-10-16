3b3d5f877eed14aca312601c1d3ac04b5fe06c6f
Kwon <finalsoul1@gmail.com>
2018-10-16 21:36:43 +0900

    vuex

---
+++ vuex/vuex-running/src/counter.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+
+  <section class="counter">
+    <div>
+      <hr>
+      Child counter : {{ getCounter }} <br>
+      <button>+</button>
+      <button>-</button>
+    </div>
+  </section>
+
+</template>
+
+<script lang="js">
+import {mapGetters} from 'vuex'
+
+export default {
+  // Parent 에서 넘겨준 counter 속성을 passedCounter 로 받음
+  // props: ['passedCounter']
+  computed: mapGetters([
+    'getCounter'
+  ]),
+}
+</script>
+
+<style scoped>
+
+</style>
